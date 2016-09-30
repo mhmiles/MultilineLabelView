69e2aef88f62911427960ddc22c80c134ac0defa
Miles Hollingsworth <mhmiles@me.com>
2016-09-30 12:21:10 -0700

    Initial Commit

---
+++ MultilineLabelView/MultilineLabelView.h
@@ -0,0 +1,19 @@
+//
+//  MultilineLabelView.h
+//  MultilineLabelView
+//
+//  Created by Miles Hollingsworth on 9/30/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultilineLabelView.
+FOUNDATION_EXPORT double MultilineLabelViewVersionNumber;
+
+//! Project version string for MultilineLabelView.
+FOUNDATION_EXPORT const unsigned char MultilineLabelViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultilineLabelView/PublicHeader.h>
+
+
