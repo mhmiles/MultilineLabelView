56a6e88dc2076234d002b0076489746ab6afe790
Miles Hollingsworth <mhmiles@me.com>
2016-09-30 12:32:37 -0700

    First commit

---
+++ MultilineLabelView/MultilineLabelView.swift
@@ -0,0 +1,171 @@
+//
+//  MultilineLabelView.swift
+//  Master Control
+//
+//  Created by Miles Hollingsworth on 9/11/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+import UIKit
+import ReactiveSwift
+import NotificationCenter
+import enum Result.NoError
+
+public class MultilineLabelView: UIView {
+    fileprivate var stackView = UIStackView() {
+        didSet {
+            stackView.axis = .vertical
+            stackView.alignment = .fill
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            
+            addSubview(stackView)
+            stackView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                oldValue.alpha = 0.0
+                }, completion: { (success) in
+                    oldValue.removeFromSuperview()
+            })
+        }
+    }
+    
+    fileprivate var labelCenterConstraints: [NSLayoutConstraint]?
+    
+    public var dataSource = MutableProperty<MultilaneLabelViewDataSource?>(nil)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureLabels()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        configureLabels()
+    }
+    
+    fileprivate var deallocDisposable: ScopedDisposable<AnyDisposable>?
+    
+    private func configureLabels() {
+        dataSource.producer.flatMap(.latest, transform: { dataSource -> SignalProducer<[NSAttributedString], NoError> in
+            return dataSource?.stringsProducer ?? SignalProducer(value: [NSAttributedString]())
+        }).skipRepeats({ current, previous in
+            let extractString: (NSAttributedString) -> String = {
+                $0.string
+            }
+            
+            return current.map(extractString).elementsEqual(previous.map(extractString))
+        }).map({ strings -> [UILabel] in
+            strings.map { string in
+                let label = UILabel()
+                label.translatesAutoresizingMaskIntoConstraints = false
+                
+                let mutableAttributedString = NSMutableAttributedString(attributedString: string)
+                mutableAttributedString.addAttributes([
+                    NSForegroundColorAttributeName: UIColor.white
+                    ], range: NSMakeRange(0, string.length))
+                
+                label.attributedText = mutableAttributedString
+                
+                return label
+            }
+        }).startWithValues(configureNewStackView)
+        
+        let disposable = NotificationCenter.default.rac_notifications(forName: Notification.Name.UIApplicationWillEnterForeground).startWithValues { _ in
+            self.animationProducer?.start()
+        }
+        
+        deallocDisposable = ScopedDisposable(disposable)
+    }
+    
+    private var animationDisposable: Disposable?
+    private var animationProducer: SignalProducer<(), NoError>?
+    
+    private func configureNewStackView(labels: [UILabel]) {
+        animationDisposable?.dispose()
+        
+        self.stackView = UIStackView()
+        
+        let stackCenterXConstraint = stackView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: frame.width)
+        addConstraint(stackCenterXConstraint)
+        addConstraint(stackView.centerYAnchor.constraint(equalTo: centerYAnchor))
+        
+        if labels.count == 0 {
+            return
+        }
+        
+        let viewsTuples = labels.map { ($0, UIView()) }
+        let animationSpeed = CGFloat(40)
+        
+        let animationProducers = viewsTuples.flatMap { label, view -> SignalProducer<(), NoError>? in
+            view.isOpaque = false
+            view.translatesAutoresizingMaskIntoConstraints = false
+            
+            view.addSubview(label)
+            
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label]|",
+                                                               options: [],
+                                                               metrics: nil,
+                                                               views: ["label": label]))
+            
+            if let attributedStringWidth = label.attributedText?.size().width , attributedStringWidth > bounds.width {
+                let leadingConstraint = label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10)
+                leadingConstraint.isActive = true
+                
+                let labelCopy = UILabel()
+                labelCopy.translatesAutoresizingMaskIntoConstraints = false
+                labelCopy.attributedText = label.attributedText
+                
+                view.addSubview(labelCopy)
+                
+                label.bottomAnchor.constraint(equalTo: labelCopy.bottomAnchor).isActive = true
+                labelCopy.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 30).isActive = true
+                
+                return SignalProducer<(), NoError> { observer, disposable in
+                    leadingConstraint.constant = 10
+                    view.layoutIfNeeded()
+                    
+                    UIView.animate(withDuration: TimeInterval(attributedStringWidth/animationSpeed),
+                                   delay: 3,
+                                   options: [.curveLinear],
+                                   animations: {
+                                    leadingConstraint.constant = 10-(attributedStringWidth+30)
+                                    view.layoutIfNeeded()
+                        }, completion: { success in
+                            observer.sendCompleted()
+                    })
+                }
+            } else {
+                view.centerXAnchor.constraint(equalTo: label.centerXAnchor).isActive = true
+                return nil
+            }
+        }
+        
+        viewsTuples.map({ $1 }).forEach(stackView.addArrangedSubview)
+        
+        layoutIfNeeded()
+        
+        if animationProducers.count > 0 {
+            animationProducer = SignalProducer<SignalProducer<(), NoError>, NoError>(values: animationProducers).flatMap(.merge, transform: {
+                $0
+            }).times(Int.max)
+        }
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0.0, options: [], animations: {
+            stackCenterXConstraint.constant = 0.0
+            self.layoutIfNeeded()
+        }) { (success: Bool) -> Void in
+            if success {
+                self.animationProducer?.start()
+            }
+            
+        }
+    }
+}
+
+public typealias MultilineLabelStringsProducer = SignalProducer<[NSAttributedString], NoError>
+
+public protocol MultilaneLabelViewDataSource {
+    var stringsProducer: MultilineLabelStringsProducer { get }
+}
